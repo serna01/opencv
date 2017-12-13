b83f13f34f70b66a5c452bf9009f247633c3534e
Alejandro Serna M <serna01@outlook.com>
2017-12-12 20:30:52 -0500

    Agregar modo Evil

--- cartoonifier/cartoon.py
+++ cartoonifier/cartoon.py
@@ -5,7 +5,7 @@ def cartoonifyImg(frame):
     #Mascara
     gris = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     filtro_ruido = 5
-    filtro_bordes = 130
+    filtro_bordes = 10
     #eliminar ruidos
     filtro1 = cv2.medianBlur(gris, filtro_ruido)
     #buscar bordes
@@ -30,8 +30,45 @@ def cartoonifyImg(frame):
 
     filtro = cv2.cvtColor(filtro3, cv2.COLOR_GRAY2BGR)
 
-    return cv2.addWeighted(big,0.9,filtro,0.3,0.5)
+    return cv2.addWeighted(big,0.99,filtro,0.13,0.5)
 
+def evilImg(frame):
+    # Mascara
+    gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    filtro_ruido = 7
+    # eliminar ruidos
+    filtro1 = cv2.medianBlur(gris, filtro_ruido)
+
+    #Filtro Scharr
+    edges = cv2.Scharr(filtro1,cv2.CV_8U,1,0)
+    print type(edges)
+    print edges.shape
+    edges2 = cv2.Scharr(filtro1,cv2.CV_8U,0,1)
+    edges3 = cv2.add(edges, edges2)
+    print type(edges)
+    print edges.shape
+    print edges
+    filtro_evil = 6
+    edges4 = cv2.threshold(edges3, filtro_evil, 255, cv2.THRESH_BINARY_INV)[1]
+    edges5 = cv2.medianBlur(edges4, 7)
+
+    # Original
+    width, height, dim = frame.shape
+
+    smallWidth = width / 4
+    smallHeight = height / 4
+    smallImg = cv2.resize(frame, (smallHeight, smallWidth), cv2.INTER_LINEAR)
+
+    reps = 7
+    filtro_cartoon = 5
+    for i in range(0, reps):
+        smallImg = cv2.bilateralFilter(smallImg, filtro_cartoon, sigmaColor=9, sigmaSpace=7)
+
+    big = cv2.resize(smallImg, (height, width), cv2.INTER_LINEAR)
+
+    filtro = cv2.cvtColor(edges5, cv2.COLOR_GRAY2BGR)
+
+    return cv2.addWeighted(big, 0.99, filtro, 0.5, 0)
 
 cap = cv2.VideoCapture(0)
 
@@ -39,9 +76,13 @@ while True:
     # Ret --> Verdadero o falso si fue capaz de leer la informacion de la camara
     # frame --> lee el siguiente frame del video
     ret, frame = cap.read()
-    cartoon = cartoonifyImg(frame)
-    cv2.imshow('Cartoonifier', cartoon)
-    cv2.imshow('video', frame)
+    #cartoon = cartoonifyImg(frame)
+    evil = evilImg(frame)
+
+    cv2.imshow('Original', frame)
+    #cv2.imshow('Cartoonifier', cartoon)
+    cv2.imshow('Evil', evil)
+
 
     # filtro para convertir la imagen a blanco y negro
     # gris = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
